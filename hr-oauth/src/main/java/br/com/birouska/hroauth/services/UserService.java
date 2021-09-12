d66988031db4b721103548d8aaea0c798399aeb8
Alan birouska <birouska@gmail.com>
2021-09-11 22:07:13 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/br/com/birouska/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/birouska/hroauth/services/UserService.java
@@ -2,13 +2,16 @@ package br.com.birouska.hroauth.services;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.birouska.hroauth.entities.User;
 import br.com.birouska.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 	
@@ -24,6 +27,20 @@ public class UserService {
 		}
 		
 		logger.info("Email Found: " + email);
+		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		
+		if (user == null) {
+			logger.error("Email not Found: " + username);
+			throw new UsernameNotFoundException("Email not found.");
+		}
+		
+		logger.info("Email Found: " + username);
 		
 		return user;
 	}
